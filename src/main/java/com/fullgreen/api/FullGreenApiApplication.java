e3022bd5906d712875ad3b8536e303991e05f465
uru1217 <uru1217@naver.com>
2020-12-23 22:50:45 +0900

    new start

---
+++ src/main/java/com/fullgreen/api/FullGreenApiApplication.java
@@ -0,0 +1,13 @@
+package com.fullgreen.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullGreenApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullGreenApiApplication.class, args);
+    }
+
+}
